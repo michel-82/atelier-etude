6aae2a928afabc5d728fa3ee587af65258ce0ece
michel-82 <michel.parchet@gmail.com>
2026-05-03 18:25:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        navigateFallbackDenylist: [/^\/api\//, /^\/\.netlify\//],        
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
